ed791733c87c7f4c4e350dfb29015f6f29ceda00
albertkornas <auk7@psu.edu>
2020-08-30 21:25:20 -0400

    Initial commit

---
+++ Workflow/Workflow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workflow
+//
+//  Created by Albert Kornas on 8/30/20.
+//  Copyright © 2020 Albert Kornas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
