350ee1aec3e66ed22dcd0839939149d0b02c3eba
albertkornas <auk7@psu.edu>
2020-09-22 12:50:30 -0400

    Initial commit

---
+++ Pokedex/Pokedex/Models/PokemonModel.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonModel.swift
+//  Pokedex
+//
+//  Created by Albert Kornas on 9/22/20.
+//  Copyright © 2020 Albert Kornas. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonModel {
+    
+}
+
