631382fab30180fa5038e312075ee88327bfd399
albertkornas <auk7@psu.edu>
2020-10-04 20:43:29 -0400

    Initial commit

---
+++ Campus/Campus/CampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusApp.swift
+//  Campus
+//
+//  Created by Albert Kornas on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct CampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
