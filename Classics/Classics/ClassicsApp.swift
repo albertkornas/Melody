d7fd2ececb46181e358c53e850fc13b9c0bbc1b8
albertkornas <auk7@psu.edu>
2020-10-19 20:38:27 -0400

    Initial Commit

---
+++ Classics/Classics/ClassicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicsApp.swift
+//  Classics
+//
+//  Created by Albert Kornas on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct ClassicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Classics/Classics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Classics
+//
+//  Created by Albert Kornas on 10/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
