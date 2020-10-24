321bb498d24c82640ad86958060b0d2a8335296b
albertkornas <auk7@psu.edu>
2020-10-24 11:44:30 -0400

    Gridview toggle works!!! DONE

--- Classics/Classics/Views/ClassicsListView.swift
+++ Classics/Classics/Views/ClassicsListView.swift
@@ -13,33 +13,25 @@ struct ClassicsListView: View {
     @State private var viewType: Bool = false
 
     let columns = [GridItem(.adaptive(minimum: 80))]
+
     var body: some View {
-        
+        if (viewType == false) {
             List {
-                if (filterType == nil || filterType!.rawValue == "All") {
-                ForEach(classicModel.classics.indices, id:\.self) {index in
-                    NavigationLink(destination: BookDetailView(classicsModel: classicModel, book: self.$classicModel.classics[index], ind: index)) {
-                        BookRowView(book: classicModel.classics[index])
-                    }.id(index)
-                }
-                } else if (filterType!.rawValue == "Completed Books") { //Completed books filter
-                    
-                    ForEach(classicModel.classics.indices, id:\.self) {index in
-                        if (classicModel.classics[index].read == true) {
-                        NavigationLink(destination: BookDetailView(classicsModel: classicModel, book: self.$classicModel.classics[index], ind: index)) {
-                            BookRowView(book: classicModel.classics[index])
-                        }.id(index)
-                        }
-                    }
-                } else if (filterType!.rawValue == "Currently Reading") { //Currently reading book filter
-                    
-                    ForEach(classicModel.classics.indices, id:\.self) {index in
-                        if (classicModel.classics[index].pageNum > 0 && classicModel.classics[index].read == false) {
-                        NavigationLink(destination: BookDetailView(classicsModel: classicModel, book: self.$classicModel.classics[index], ind: index)) {
-                            BookRowView(book: classicModel.classics[index])
-                        }.id(index)
-                        }
-                    }
+                collectionView(classicModel: _classicModel, filterType: filterType, viewType: viewType)
+            }
+            .navigationBarTitle("Classics", displayMode: .inline)
+            .navigationBarItems(leading:
+                                    Button(action: {self.viewType.toggle()}) {Text("Toggle Grid")}
+                ,trailing:
+                     Picker(selection: $filterType, label: Text("Filter")) {
+                         ForEach(FilterType.allCases) { filter in
+                             Text(filter.rawValue).tag(filter as FilterType?)
+                         }
+                    }.pickerStyle(MenuPickerStyle()))
+        } else {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    collectionView(classicModel: _classicModel, filterType: filterType, viewType: viewType)
                 }
             }
             .navigationBarTitle("Classics", displayMode: .inline)
@@ -51,7 +43,7 @@ struct ClassicsListView: View {
                              Text(filter.rawValue).tag(filter as FilterType?)
                          }
                     }.pickerStyle(MenuPickerStyle()))
-
+        }
     }
  
             
@@ -66,12 +58,59 @@ struct ClassicsListView: View {
 
 }
 
+struct collectionView: View {
+    @EnvironmentObject var classicModel : ClassicModel
+    let filterType: FilterType?
+    let viewType: Bool
+    var body: some View {
+        if (filterType == nil || filterType!.rawValue == "All") {
+        ForEach(classicModel.classics.indices, id:\.self) {index in
+            NavigationLink(destination: BookDetailView(classicsModel: classicModel, book: self.$classicModel.classics[index], ind: index)) {
+                BookRowView(book: classicModel.classics[index], showingGrid: viewType)
+            }.id(index)
+        }
+        } else if (filterType!.rawValue == "Completed Books") { //Completed books filter
+            
+            ForEach(classicModel.classics.indices, id:\.self) {index in
+                if (classicModel.classics[index].read == true) {
+                NavigationLink(destination: BookDetailView(classicsModel: classicModel, book: self.$classicModel.classics[index], ind: index)) {
+                    BookRowView(book: classicModel.classics[index], showingGrid: viewType)
+                }.id(index)
+                }
+            }
+        } else if (filterType!.rawValue == "Currently Reading") { //Currently reading book filter
+            
+            ForEach(classicModel.classics.indices, id:\.self) {index in
+                if (classicModel.classics[index].pageNum > 0 && classicModel.classics[index].read == false) {
+                NavigationLink(destination: BookDetailView(classicsModel: classicModel, book: self.$classicModel.classics[index], ind: index)) {
+                    BookRowView(book: classicModel.classics[index], showingGrid: viewType)
+                }.id(index)
+                }
+            }
+        }
+    }
+}
+
 
 struct BookRowView : View {
     let book : Book
+    let showingGrid: Bool
     var body: some View {
-        Text(book.title)
+        if (showingGrid == false) {
+            Text(book.title)
+        } else {
+            VStack {
+                Image(String(book.image))
+                .resizable()
+                .frame(width: frameSize, height: frameSize)
+                .cornerRadius(5)
+                Text(book.title)
+                    .multilineTextAlignment(.center)
+            }
+                
+        }
     }
+    let frameSize: CGFloat = 60
 }
 
 
