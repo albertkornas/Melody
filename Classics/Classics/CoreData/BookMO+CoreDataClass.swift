a2652327eec88d537a8f202a93a52534eda29161
albertkornas <auk7@psu.edu>
2020-10-31 10:49:39 -0400

    more core data

---
+++ Classics/Classics/CoreData/BookMO+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  BookMO.swift
+//  Classics
+//
+//  Created by Albert Kornas on 10/31/20.
+//
+
+import Foundation
+import CoreData
+
+@objc(PlayerMO)
+public class BookMO: NSManagedObject {
+
+}
